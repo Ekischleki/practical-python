498c28532df7b2251df4df982d6bcc9af1b02acf
Erik Wolfschlaeger <e.wolfschlaeger@gmail.com>
2020-06-11 12:02:34 +0200

    dies dass bankrechnung...

--- Work/zinsen.py
+++ Work/zinsen.py
@@ -3,9 +3,10 @@ tag = 0
 schulden = 0
 zinssatz =  float(input("zinssatz bidde "))
 laufzeit = float(input("wie lange ist die Laufzeit? "))
+zurückgezahlt =float(input("wie fiel hast du zurückgezahlt"))
 while tag < laufzeit:
     tag = tag +1
-    schulden = ischulden / 100 * zinssatz
+    schulden = ischulden / 100 * zinssatz - zurückgezahlt 
     ischulden = schulden + ischulden 
 
     print("die schulden von tag: " ,tag, " betragen:" ,ischulden )
